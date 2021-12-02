88781ebf79dbff895a1276907b69105abfd4ec72
Mazzaferno <95215692+Mazzaferno@users.noreply.github.com>
2021-12-02 03:47:10 -0600

    Update AutoH3Tool.bat
    removed the file explorer because drag and drop works good enough and doesn't require .NETFramework to be installed

--- AutoH3Tool.bat
+++ AutoH3Tool.bat
@@ -1,140 +1,131 @@
 @echo off
-COLOR 0D
-title AutoH3Tool
-set filefolder=0
-reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\.NETFramework" 2>nul
-cls
-if errorlevel 1 (
-    echo +-------------------------------------+
-    echo  ERROR .NET Framework in not detected
-    echo +-------------------------------------+
-    goto choice
-) else (
-    goto intro
-)
-
-:choice
-set /P c=open .netframework download page[y/n]?
-if /i %c%==y goto download
-if /i %c%==n goto end
-
-:download
-start "" https://dotnet.microsoft.com/download/dotnet-framework
-pause
-goto end
-
 :intro
+set TD=
+set scnr=0
+set file=null
+cls
+mode con: cols=75 lines=50
+color 0D
+echo "                           _  --  _                        
+echo "    ___         __        / \|  |/ \  __________  ____  __ 
+echo "   /   | __  __/ /_____   \  |  |  / /_  __/ __ \/ __ \/ / 
+echo "  / /| |/ / / / __/ __ \ --  |  |  -- / / / / / / / / / /  
+echo " / ___ / /_/ / /_/ /_/ /\    '--'    / / / /_/ / /_/ / /___
+echo "/_/  |_\__,_/\__/\____/  '--      --'_/  \____/\____/_____/
+echo "                           /__/\__\                         
 echo +-----------------------------------------------------------------------+
 echo         AutoH3Tool is used to import and export H3 Tags for H3EK
 echo             this tool automates H3EK's Tool.exe commands
-echo +------------------------------------------------------------------------------------------------+
-echo developed by Mazzaferno
-echo +-----------------------------------------------+
-echo !make sure you have .NET framework installed!
-echo +-----------------------------------------------+
-set file=null
-if exist tags\ (
+echo +-----------------------------------------------------------------------+
+echo.
+echo +--------------select command------------+
+echo [1] - structure
+echo [2] - build-cache-file
+echo [3] - import model - Bulk - Render - Physics - Collision
+echo [4] - model-animations
+echo [5] - model-animations-uncompressed
+echo [6] - extract-import-info
+echo [7] - Bitmaps - bitmap_single
+echo +----------------------------------------+
+set /p c=Choice:
+	if %c%==1 goto 1
+	if %c%==2 goto 2
+	if %c%==3 goto 3
+	if %c%==4 goto 4
+	if %c%==5 goto 5
+	if %c%==6 goto 6
+	if %c%==7 goto 7
+	if %c% gtr 7  goto intro
 	
-  goto command 
-) else (
-  powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('autotool must be in (steamapps\common\H3EK) directory', 'Error', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
-goto end
-)
-pause
+		:1
+		set command=structure
+		set example=\data\levels\multi\test\structure\test.ass
+		cd data
+		goto selection
+		:2
+		set command=build-cache-file
+		set example=\tags\levels\multi\test\test.scenario
+		cd tags
+		set TD=tags\
+		goto selection		
+		:3
+		echo +------------------------------+
+		echo [1] - bulk-import-model-folder
+		echo [2] - Render
+		echo [3] - physics
+		echo [4] - collision
+		echo +------------------------------+
+		set /p c=Choice:
+		set example=data\objects\multi\test\render\render(.jms/.jmi)
+			
+			if %c%==1 set command=bulk-import-model-folder
+				if %c%==1 set example=data\objects\multi\test
+				if %c%==1 set scnr=1
+			if %c%==2 set command=Render
+			if %c%==3 set command=physics
+			if %c%==4 set command=collision
+			if %c% gtr 4 goto intro
 
+		cd data
+		goto selection
+		:4
+		set command=model-animations
+		set example=
+		goto selection
+		:5
+		set command=model-animations-uncompressed
+		set example=
+		goto selection
+		:6
+		set command=extract-import-info
+		set example=tags\objects\weapons\example\example(.render)(.collision)(.physics)(.scenario_structure_bsp)
+		cd tags
+		set TD=tags\
+		goto selection
+		:7
+		echo +------------------+
+		echo [1] - Bitmap_single
+		echo [2] - Bitmaps "bulk"
+		echo +------------------+
+		set /p c=Choice:
+			if %c%==1 set command=Bitmap_single
+			if %c%==2 set command=Bitmaps
+			if %c%==1 set example=data\levels\multi\chill\bitmaps\example(.tiff/.tif)
+			if %c%==2 set example=data\levels\multi\chill\bitmaps
+			cd data
+		goto selection
 
-
-:: command choice
-
-:Command
-echo [1] export
-echo [2] import
-set /P c=Type:
-cls
-	if %c%==1 goto export
-	if %c%==2 goto import
-	goto intro
-:export
-	echo             select one of the following file types to export
-	echo +--------------------------------------------------------------------------+
-	echo    .render_model, .collision_model, .physics_model, .scenario_structure_bsp
-	echo +--------------------------------------------------------------------------+
-	echo files must be within "...H3EK\tags" directory 
-	echo +--------------------------------------------------------------------------+
-	set command=extract-import-info
-	set TD=tags\
-	goto start
-:import
-	echo                       [import type]
-	echo +-----------------------------------------------------------------------------------------------------+
-	echo [1] Structure  ".ass to BSP"
-	echo     usage: "..H3EK\data\level\multi\test\structure\test.ass"
-	echo +-----------------------------------------------------------------------------------------------------+
-	echo [2] Bulk-import-model-folder
-	echo  "compiles the render, collision, and physics directory of a model"
-	echo    usage: "..H3EK\data\mystuff\mymodel"
-	echo   "\mymodel" should contain folders named \collision\ \render\ \physics\ containing appropriate.JMS files!!
-	echo +-----------------------------------------------------------------------------------------------------+
-	set c=0
-	set /P c=Type:
-		if %c%==1 goto structure
-		if %c%==2 goto bulk-import-model-folder
-	set c=0
-	cls
-	if %c%==0 goto import
-	goto import
-	echo %command%
-	pause
-:structure
-	set command=structure
-	set TD=
-	set filefolder=1
-	goto start
-:bulk-import-model-folder
-	cls
-	set command=bulk-import-model-folder
-	set TD=
-	set filefolder=1
-	goto start
-
-:: command choice end
-
-
-
-:start
-if %filefolder%==1 goto folder
-cd tags
-call :filedialog file
-exit /b
-:filedialog :: &file
-setlocal
-set dialog="about:<input type=file id=FILE><script>FILE.click();new ActiveXObject
-set dialog=%dialog%('Scripting.FileSystemObject').GetStandardStream(1).WriteLine(FILE.value);
-set dialog=%dialog%close();resizeTo(0,0);</script>"
-for /f "tokens=* delims=" %%p in ('mshta.exe %dialog%') do set "file=%%p"
-endlocal  & set %1=%file%
-goto exe
-:: execute command
-
-:folder
+:selection
+set file=null
+echo Command: [%command%]
 echo +-------------------------------------------------------+
-echo drag file or folder here and press enter
-echo usage: ...H3EK\data\mystuff\mymodel
+echo         DRAG FILE OR FOLDER HERE AND PRESS ENTER
+echo ---------------------------------------------------------
+echo usage: ...%example%
 echo +-------------------------------------------------------+
 set /p file=
-cd data
+
 :exe
+COLOR 0B
 cls
 if %file%==null goto nofile
-set folder=%cd%
+
+::removes unused portion of %file% C:\H3EK\tags\objects\weapons >becomes> objects\weapons
+
 call set output=%%file:%cd%=%%
+
 set output=%output:~1%
+
+	if %scnr%==1 set output=%output:~0,-9%
+
+:tool
+
+echo starting command:Tool %command% %TD%%output%
 cd ../
-COLOR 0B
-echo %command% %TD%%output%
-tool %command% %TD%%output%
+echo tool %command% %TD%%output%
 pause
-exit
+goto intro
+
 
 :nofile
 COLOR 0C
@@ -142,7 +133,18 @@ echo +--------------------+
 echo  No file was selected
 echo +--------------------+
 echo %command% %TD%%output%
+echo "     _  --  _
+echo "    / \|  |/ \
+echo "    \  |  |  /
+echo "   --  |  |  -- 
+echo "  \    '--'    /
+echo "   '--      --'
+echo "     /__/\__\
+echo "
+cd ../
 pause
+cls
+goto intro
 
 :end
 exit
